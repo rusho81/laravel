9df04807700a2073f20331e0f40d00d7749c2320
Quazi Tariul Alam <rushokazi@gmail.com>
2023-01-12 23:48:58 +0600

    First commit

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+$name = "Bangladesh";
+$age = 21;
+$word = "age";
+$question = "How are you?";
+
+echo $$word;
+echo "\n";
+echo "Hello $name $question\n";
+echo "Hello {$name} {$question}";
