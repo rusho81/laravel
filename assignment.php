0dedea95abbc8e45255a6f1bc85e10e6813fd369
Quazi Tariul Alam <rushokazi@gmail.com>
2023-02-13 22:02:04 +0600

    Module 3 assignment

--- assignment.php
+++ assignment.php
@@ -1,11 +1,11 @@
 <?php
-// //1. Write a Reusable  PHP Function that can check Even & Odd Number And Return Decision
-// function evenOdd(int $num){
-//     if($num % 2 == 0){
-//         return "Even Number";
-//     }
-//     return "Odd Number";
-// }
+//1. Write a Reusable  PHP Function that can check Even & Odd Number And Return Decision
+function evenOdd(int $num){
+    if($num % 2 == 0){
+        return "Even Number";
+    }
+    return "Odd Number";
+}
 
 //2. 1+2+3...…….100  Write a loop to calculate the summation of the series
 $result = 0;
