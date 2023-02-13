8b400dae4d49a361aac9d865bb296459c63c933a
Quazi Tariul Alam <rushokazi@gmail.com>
2023-02-13 21:57:43 +0600

    Module 3 assignment

---
+++ assignment.php
@@ -0,0 +1,15 @@
+<?php
+// //1. Write a Reusable  PHP Function that can check Even & Odd Number And Return Decision
+// function evenOdd(int $num){
+//     if($num % 2 == 0){
+//         return "Even Number";
+//     }
+//     return "Odd Number";
+// }
+
+//2. 1+2+3...…….100  Write a loop to calculate the summation of the series
+$result = 0;
+for($i = 1; $i <= 100; $i++){
+    $GLOBALS['result'] += $i;
+}
+echo $result;
--- test.php
+++ test.php
@@ -1,55 +1,31 @@
 <?php
-//include_once "functions.php";
-/**
- * Determines if an argument is even or odd.
- */
+$person = array(
+    "first_name" => "Faisal",
+    "last_name"  => "Ahmed",
+    "address"    => "123 Main Street",
+    "city"       => "Beverly Hills",
+    "state"      => "CA",
+    "zip_code"   => "90210",
+);
 
-// function add($x, $y, int ...$number):int{
-//     $result = 0;
-//     for($i=0;$i<count($number);$i++){
-//         $result += $number[$i];
-//     }
-//     return $result;
-// }
-// echo add(5.3,4,6,6);
-
-// function printNumber($counter, $end, $stepping=1){
-//     if($counter>$end){
-//         return;
-//     }
-//     echo $counter."\n";
-//     $counter+=$stepping;
-//     printNumber($counter, $end, $stepping);
-// }
-// printNumber(21,37,6);
-// function fibonnaci($old, $new, $end){
-
-//     static $start;
-//     $start = $start ?? 1;
-//     if($start>$end){
-//         return;
-//     }
-//     $start++;
-//     echo $old." ";
-//     $_temp = $old + $new;
-//     $old = $new;
-//     $new = $_temp;
-//     fibonnaci($old, $new, $end);
-// }
-// fibonnaci(0,1,10);
-function calculator(int $x, int $y, $method="sum"){
-    $result = ("sum"==$method)?($x+$y):(("sub"==$method)?($x-$y):(("div"==$method)?($x/$y):("Invalid")));
-    return $result;
+foreach ( $person as $attribute => $data ) {
+    $attr_nice = ucwords( str_replace( "_", " ", $attribute ) );
+    echo "{$attr_nice}: {$data}";
+    echo PHP_EOL;
 }
-echo calculator(12,5,"sub").PHP_EOL;
-echo "*******".PHP_EOL;
+print_r($person);
 
-for($i=10,$j=1;$i>0;$i--,$j++){
-    echo $i.":".$j.PHP_EOL;
-}
-echo "*******".PHP_EOL;
-for($i=6,$factorial=1;$i>0;$i--){
-    $factorial *=$i;
-    echo $factorial.PHP_EOL;
-}
+echo PHP_EOL;
+$prices = array( "Brand New Computer" => 2000,
+    "1 month of Lynda.com"               => 25,
+    "Learning PHP"                       => null );
 
+foreach ( $prices as $item => $price ) {
+    echo "{$item}: ";
+    if ( is_int( $price ) ) {
+        echo "$" . $price;
+    } else {
+        echo "priceless";
+    }
+   echo PHP_EOL;
+}
