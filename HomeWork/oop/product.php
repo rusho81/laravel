f74f5e193abd9a3c693808c45696d93ae5bf8364
Quazi Tariul Alam <rushokazi@gmail.com>
2023-02-22 22:19:10 +0600

    HomeWork OOP

---
+++ HomeWork/oop/product.php
@@ -0,0 +1,20 @@
+<?php
+class Product{
+    private $name;
+    private $price;
+    private $description;
+    public function __construct($productName, $productPrice, $productDes)
+    {
+        $this->name = $productName;
+        $this->price = $productPrice;
+        $this->description = $productDes;
+    }
+    public function display_info(){
+        echo "Product Name: {$this->name}".PHP_EOL;
+        echo "Product Price: {$this->price}".PHP_EOL;
+        echo "Product Description: {$this->description}".PHP_EOL;
+    }
+}
+
+$pod1 = new Product("Apple", 10, "Info about fruits");
+$pod1->display_info();
--- test.php
+++ test.php
@@ -105,4 +105,49 @@
 // AB;
 // echo $string;
 // $arr = array('apple','banana','cherry');
-// echo implode(',',$arr);
+// echo implode(',',$arr);echo $_SERVER['PHP_SELF'];
+class RGB{
+    private $color;
+    private $red;
+    private $green;
+    private $blue;
+    function __construct($colorCode = '')
+    {
+        $this->color = ltrim($colorCode,'#');
+        $this->parseColor();
+    }
+    function getColor(){
+        return $this->color;
+    }
+    function getRGBColor(){
+        return array($this->red, $this->green, $this->blue);
+    }
+    function readRGBColor(){
+        echo "Red={$this->red}\nGreen={$this->green}\nBlue= {$this->blue}";
+    }
+    function setColor($colorCode){
+        $this->color = ltrim($colorCode,"#");
+        $this->parseColor();
+    }
+    function getRed(){
+        return $this->red;
+    }
+    function getGreen(){
+        return $this->green;
+    }
+    function getBlue(){
+        return $this->blue;
+    }
+    private function parseColor(){
+        if($this->color){
+            list($this->red, $this->green, $this->blue) = sscanf($this->color,'%02x%02x%02x');
+        }else{
+            list($this->red, $this->green, $this->blue) = array(0,0,0);
+        }
+        
+    }
+}
+$myColor = new RGB("#ff0000");
+$myColor->getColor();
+
+
